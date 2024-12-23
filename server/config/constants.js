afaec3bb99764bb8b3fae15e7928ec084ef1b845
timncox <timcox@gmail.com>
2024-12-22 22:55:01 -0500

    main

--- server/config/constants.js
+++ server/config/constants.js
@@ -4,7 +4,7 @@ export const SERVER_CONFIG = {
     'http://localhost:5173',
     'http://127.0.0.1:5173', 
     'http://[::1]:5173',
-    'http://192.168.1.104:5173'
+    'https://photo-poetry-app.netlify.app'
   ],
   MAX_REQUEST_SIZE: '50mb',
   CORS_MAX_AGE: 86400,
